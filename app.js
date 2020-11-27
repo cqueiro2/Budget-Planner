9d1238b6753c36a5856dd383808866f076167db1
cqueiro2 <cqueiro2@hotmail.com>
2020-11-27 12:13:40 -0300

    pull

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const reasonInput = document.querySelector('#input-reason')
 const amountInput = document.querySelector('#input-amount')
 const cancelBtn = document.querySelector('#btn-cancel')
 const confirmBtn = document.querySelector('#btn-confirm')
+const expensesList = document.querySelector('#expenses-list')
+const totalExpensesOutput= document.querySelector('#total-expenses')
+const alertCtrl = document.querySelector('ion-alert-controller')
+
+let totalExpenses = 0
+
+const clear = () => {
+    reasonInput.value = ''
+    amountInput.value=''
+}
 
 confirmBtn.addEventListener('click' , () =>{
      const enteredReason = reasonInput.value
@@ -12,9 +22,28 @@ confirmBtn.addEventListener('click' , () =>{
          enteredAmount <= 0 || 
          enteredAmount.trim().length <= 0
      ){
+         alertCtrl
+           .create.alertElement({
+          message:'Please enter valid reason and amount!',
+          header:'Invalid inputs',
+          buttons:['Okay']
+        })
+           .then(alertElement => {
+            alertElement.present()
+        })
         return
     
     }
-    console.log(enteredReason, enteredAmount)
+     const newItem = document.createElement('ion-item')
+   newItem.textContent = enteredReason  + ':  $' + enteredAmount
 
+   expensesList.appendChild(newItem)
+
+   totalExpenses += +enteredAmount
+
+   totalExpensesOutput.textContent = totalExpenses
+
+   clear()
 })
+
+cancelBtn.addEventListener('click', clear)
